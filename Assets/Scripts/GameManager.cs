74b60f4b82309ba9af5d53ddf568616d741563df
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-03-04 11:49:27 +0000

    Game over and lifes added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject star;
+
+    public List<GameObject> life = new List<GameObject>();
+
+    void Start()
+    {
+        for (int i=0; i<5; i++)
+        {
+            insertLife();
+        }
+    }
+
+    public void insertLife()
+    {
+        Vector3 position = new Vector3((life.Count - 12), 0,-2);
+
+        GameObject instance = Instantiate(star, position, Quaternion.identity) as GameObject;
+        instance.transform.SetParent(gameObject.transform);
+        instance.transform.Rotate(new Vector3(300, 0, 0));
+        life.Add(instance);
+    }
+
+    public void loseLife()
+    {
+        GameObject lastStar = life[life.Count - 1];
+        life.Remove(lastStar);
+        Destroy(lastStar);
+
+        if (life.Count == 0)
+        {
+            print("Game over");
+            
+            // Application.LoadLevel("GameOver"); depricated method
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/HitCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitCollider : MonoBehaviour {
+
+    public GameObject gamemanager;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            print("Hit");
+            GameManager gm = gamemanager.GetComponent<GameManager>();
+            gm.loseLife();
+        }
+    }
+}
