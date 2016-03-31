d52991c905c51d546cd6b7cf028d291ab98c5229
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-03-31 13:26:41 +0100

    background animation

---
+++ Assets/Scripts/background/BackgroundScroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeY;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+        
+
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);        
+
+        transform.position = startPosition + Vector3.up * newPosition;
+
+    }
+}
