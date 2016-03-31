a991d068907ec6b94da22c8dcb0d56afdadc6f97
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-03-31 15:23:21 +0100

    break asteroids with your arms

---
+++ Assets/Scripts/HitColliderDef.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitColliderDef : MonoBehaviour {
+
+    public GameObject gamemanager;
+    public GameObject explosion;
+    bool _triggered;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (_triggered)
+        {
+            return;
+        }
+        _triggered = true;
+
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            //print("Hit");
+            Destroy(other.gameObject);
+
+            if (explosion != null)
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+            }
+
+            GameManager gm = gamemanager.GetComponent<GameManager>();
+            // add score
+            
+        }
+    }
+
+    void OnTriggerExit(Collider hit)
+    {
+        if (!_triggered)
+        {
+            return;
+        }
+        _triggered = false;
+        //print("exit");
+    }
+}
