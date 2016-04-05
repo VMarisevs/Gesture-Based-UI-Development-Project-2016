628a17dce0be3a4e9567218f7d41071d751a7e83
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-04-05 02:22:17 +0100

    adding gravity to a bird, and fixing the cam behind it

---
+++ Assets/Scripts/FlappyBird/BirdGravity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdGravity : MonoBehaviour {
+
+    Vector3 velocity = Vector3.zero;
+
+    public Vector3 gravity;
+
+	void FixedUpdate () {
+        velocity += gravity;
+
+        transform.position += velocity * Time.deltaTime;
+	}
+}
