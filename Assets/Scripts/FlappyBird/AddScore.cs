65186ac8a4b3e05bc202154dc0a5c74f9dbbf5f6
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-04-05 12:38:04 +0100

    score counter

---
+++ Assets/Scripts/FlappyBird/AddScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+
+    public GameScore score;
+
+    void OnTriggerEnter(Collider other)
+    {
+        score.addScore();
+    }
+}
---
+++ Assets/Scripts/FlappyBird/GameScore.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour {
+
+    private int score = 0;
+
+    public Text gameScore;
+
+    public void addScore()
+    {
+        score += 1;
+        gameScore.text = score.ToString();
+    }
+}
