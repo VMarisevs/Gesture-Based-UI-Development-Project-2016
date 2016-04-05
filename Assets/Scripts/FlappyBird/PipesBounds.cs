6ab2f025a4832e5b0e904544ec97002983930076
Vladislavs Marisevs <vmarisevs@gmail.com>
2016-04-05 10:47:27 +0100

    bound destroyer

---
+++ Assets/Scripts/FlappyBird/PipesBounds.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipesBounds : MonoBehaviour {
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
